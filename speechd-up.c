a197ac4e724e98a5f7fe6022c654527876288baa
Hynek Hanke <hanke@brailcom.org>
2006-01-25 12:17:33 +0000

    * speechd-up.c: Incorporated Michael McDonalds patch for index marking support (not included before into CVS because it would cause dependency on non-released-yet version of Dispatcher)
    * speechd-up.c: Handle both the old and the new device type for
    /dev/softsynth (O_RDWR and O_RDONLY). (not included before into
    CVS because it would cause dependency on non-released-yet version
    of Dispatcher)

--- speechd-up.c
+++ speechd-up.c
@@ -19,7 +19,7 @@
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- * $Id: speechd-up.c,v 1.9 2005-10-10 10:34:41 hanke Exp $
+ * $Id: speechd-up.c,v 1.10 2006-01-25 12:17:33 hanke Exp $
  */
 
 #include <stdio.h>
@@ -44,6 +44,8 @@
 #define DTLK_CMD 1
 
 int fd;
+fd_set fd_list;
+fd_set fd_write;
 SPDConnection *conn;
 
 FILE *logfile;
@@ -64,16 +66,21 @@ DBG(int level, char *format, ...)
 		
     va_start(args, format);
     {
-      {
+	{
 	/* Print timestamp */
 	time_t t;
 	char *tstr;
 	t = time(NULL);
 	tstr = (char*) strdup(ctime(&t));
+	if (tstr == NULL){
+	    fprintf(stderr, "strdup(ctime) failed, can't log");
+	    return;
+	}
 	/* Remove the trailing \n */
 	assert(strlen(tstr)>1);
 	tstr[strlen(tstr)-1] = 0;
 	fprintf(logfile, "[%s] speechd: ", tstr);
+	free(tstr);
       }
       for(i=1;i<level;i++){
 	fprintf(logfile, " ");
@@ -83,8 +90,13 @@ DBG(int level, char *format, ...)
       fflush(logfile);
     }
     va_end(args);
-
   }				
+}
+
+void index_marker_callback(size_t msg_id, size_t client_id, SPDNotificationType type, char *index_mark)
+{
+  DBG(5,"Index Mark Callback");
+  int cw = write(fd, index_mark, sizeof(index_mark));
 }
 
 #define FATAL(status, format...) { DBG(0, format); exit(status); }
@@ -94,6 +106,9 @@ speechd_init()
 {
   conn = spd_open("speakup", "softsynth", "test", SPD_MODE_THREADED);
   if (conn == 0) FATAL(1, "ERROR! Can't connect to Speech Dispatcher!");
+  conn->callback_im=index_marker_callback;
+  if (spd_set_notification_on(conn, SPD_INDEX_MARKS )==-1)
+	DBG(1,"Error turning on Index Mark Callback");
 }
 
 void
@@ -135,9 +150,9 @@ process_command(char command, unsigned int param, int pm)
 			DBG(5, "[punctuation none]", val);
 			ret = spd_set_punctuation(conn, SPD_PUNCT_NONE);
 			break;
-		default: perror("ERROR: Invalid punctuation mode!");
+		default: DBG(1, "ERROR: Invalid punctuation mode!");
 		}
-		if (ret == -1) perror("ERROR: Can't set punctuation mode");
+		if (ret == -1) DBG(1, "ERROR: Can't set punctuation mode");
 		break;
 
 	case 'o': /* set voice */
@@ -199,17 +214,21 @@ process_command(char command, unsigned int param, int pm)
 		assert((val >= -100) && (val <= +100));
 		DBG(5, "[rate %d, param: %d]", val, param);
 		ret = spd_set_voice_rate(conn, val);
-		if (ret == -1) perror("ERROR: Invalid rate!");
+		if (ret == -1) DBG(1, "ERROR: Invalid rate!");
 		break;
 
+	case 'f':
+	    DBG(3, "WARNING: [frequency setting not supported,"
+		"use rate instead]");
+	    break;
+
 	case 'v': 
-		DBG(3, "[volume setting not supported yet]", val);
+		DBG(3, "[volume setting not supported yet]");
 		break;
 
 	case 'x': 
-		DBG(3, "[tone setting not supported]", val);
+		DBG(3, "[tone setting not supported]");
 		break;
-
 	default:
 		DBG(3, "ERROR: [%c: this command is not supported]", command);
 	}
@@ -219,7 +238,7 @@ process_command(char command, unsigned int param, int pm)
 int
 parse_buf(char *buf, size_t bytes)
 { 
-  char helper[16];
+  char helper[20];
   char cmd_type;
   int  n, m;
   unsigned int param;
@@ -229,6 +248,9 @@ parse_buf(char *buf, size_t bytes)
   iconv_t cd;
   int i;
   int enc_bytes;
+  char *start_tag="<speak>";
+  char *end_tag="</speak>\x00";
+  //char *mark_tag="<mark name=\"%s\">";
   char *pi, *po;
   size_t bytes_left = BUF_SIZE;
   size_t in_bytes;
@@ -238,12 +260,16 @@ parse_buf(char *buf, size_t bytes)
   assert (bytes <= BUF_SIZE);
 
   cd = iconv_open("utf-8", SPEAKUP_CODING);
-  if (cd == (iconv_t) -1) FATAL(1, "Requested character set conversion not possible"
-			"by iconv: %s!", strerror(errno));
+  if (cd == (iconv_t) -1)
+      FATAL(1, "Requested character set conversion not possible"
+	    "by iconv: %s!", strerror(errno));
 
   pi = buf;
   po = text;
   m = 0;
+
+  strcpy(text,start_tag);
+  po=text+strlen(start_tag);
 
   for(i = 0; i < bytes; i++)
     {
@@ -253,23 +279,15 @@ parse_buf(char *buf, size_t bytes)
 	  spd_cancel(conn);
 	  DBG(5, "[stop]");
 	  pi = &(buf[i+1]);
-	  po = text;
+	  //po = text;
+          strcpy(text,start_tag);
+          po=text+strlen(start_tag);
 	  m = 0;
 	}
 
       /* Process a command */
       else if (buf[i] == DTLK_CMD)
-	{
-
-	  /* If there is some text before this command, say it */
-	  if (m > 0)
-	    {	     
-	      DBG(5, "text: |%s|", text);
-	      DBG(5, "[speaking (2)]");
-	      spd_say(conn, SPD_MESSAGE, text);
-	      m = 0;
-	    }
-      
+      {      
 	  /* If the digit is signed integer, read the sign.  We have
 	     to do it this way because +3, -3 and 3 seem to be three
 	     different thinks in this protocol */
@@ -288,11 +306,33 @@ parse_buf(char *buf, size_t bytes)
 	    cmd_type = buf[i];
 	  }
 
-	  /* Now when we have the command (cmd_type) and it's
-	     parameter, let's communicate it to speechd */
-	  process_command(cmd_type, param, pm);
-	  pi = &(buf[i+1]);
-	  po = text;
+	  if (cmd_type=='i')
+	  {
+		DBG(5, "Insert Index %d",param);
+		sprintf(helper,"<mark name=\"%d\"/>",param);
+		strcpy(po,helper);
+	 	 pi = &(buf[i+1]);
+		po+=strlen(helper);
+		*po=0;
+	  }
+	  else
+	  {
+            /* If there is some text before this command, say it */
+            if (m > 0)
+            {
+              DBG(5, "text: |%s|", text);
+              DBG(5, "[speaking (2)]");
+              strcat(text,end_tag);
+              spd_say(conn, SPD_MESSAGE, text);
+              m = 0;
+            }
+	    /* Now when we have the command (cmd_type) and it's
+	       parameter, let's communicate it to speechd */
+	    process_command(cmd_type, param, pm);
+	    pi = &(buf[i+1]);
+            strcpy(text,start_tag);
+            po=text+strlen(start_tag);
+	  }
 	}
       else
 	{
@@ -312,11 +352,11 @@ parse_buf(char *buf, size_t bytes)
 
   /* Finally, say the text we read from /dev/softsynth*/
   assert(m>=0);
-  
+
   if (m != 0){
+    strcpy(po,end_tag);  
     DBG(5,"text: |%s %d|", text, m);
     DBG(5, "[speaking]");
-    // text[m] = 0;
     spd_say(conn, SPD_MESSAGE, text);
     DBG(5,"---");
   }
@@ -414,7 +454,7 @@ main (int argc, char *argv[])
   int i;
   size_t chars_read;
   char buf[BUF_SIZE];
-  fd_set fd_list;
+  int ret;
 
   options_set_default();
   options_parse(argc, argv);
@@ -448,9 +488,23 @@ main (int argc, char *argv[])
   DBG(1,"Speechd-speakup starts!");
 
   if (!PROBE_MODE){
-      if ((fd = open(SPEAKUP_DEVICE, O_RDONLY)) < 0) {
-	  FATAL(2, "ERROR! Unable to open soft synth device (%s)\n", SPEAKUP_DEVICE);
-	  return -1;
+      if ((fd = open(SPEAKUP_DEVICE, O_RDWR)) < 0) {
+	  DBG(1,"Error while openning the device in read/write mode %d,%s",
+	      errno,strerror(errno));
+	  DBG(1,"Trying to open the device in the old way.");
+	  if ((fd = open(SPEAKUP_DEVICE, O_RDONLY)) < 0) {
+	      DBG(1,"Error while openning the device in read mode %d,%s",
+		  errno,strerror(errno));
+	      FATAL(2, "ERROR! Unable to open soft synth device (%s)\n",
+		    SPEAKUP_DEVICE);
+	      return -1;
+	  }else{
+	      DBG(1,"It seems you are using an older version of Speakup "
+		  "that doesn't support index marking. This is not a problem "
+		  "but some more advanced Speakup's function might not work "
+		  "until you upgrade Speakup.");
+	  }
+	  
       }
       if ( fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK) == -1) {
 	  FATAL(5, "fcntl() failed");
@@ -469,7 +523,13 @@ main (int argc, char *argv[])
       DBG(1, "SpeechD-Up is terminating correctly in probe mode");
       return 0;
   }
-      
+
+  ret = spd_set_data_mode(conn,SPD_DATA_SSML);
+  if (ret){
+      DBG(1, "This version of Speech Dispatcher doesn't support SSML mode.\n"
+	  "Please use a newer version of Speech Dispatcher (at least 0.5)");
+      FATAL(6, "SSML not supported in Speech Dispatcher");
+  }
 
   while (1){
     FD_ZERO(&fd_list);
