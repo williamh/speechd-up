63e3d0ccd4f1d7ebef5249d468bfbb7e84f19617
William Hubbs <w.d.hubbs@gmail.com>
2010-10-02 08:53:03 -0500

    clean up implicit declarations

--- log.c
+++ log.c
@@ -24,6 +24,7 @@
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
 #include <assert.h>
--- speechd-up.c
+++ speechd-up.c
@@ -25,6 +25,7 @@
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
