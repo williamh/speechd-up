28b1426fe9faad14a68fdf543e19e61bed38d9f6
William Hubbs <w.d.hubbs@gmail.com>
2010-10-03 16:28:06 -0500

    rework configuration handler
    The dotconf callbacks have been separated into functions for easier
    maintenance.

--- configuration.c
+++ configuration.c
@@ -36,137 +36,155 @@
 
 extern struct spd_options options;
 
-/* == CONFIGURATION MANAGEMENT FUNCTIONS */
+/*
+ * Declare the dotconf error handler and callbacks.
+ */
+static FUNC_ERRORHANDLER(errorhandler);
+static DOTCONF_CB(cb_dontInitTables);
+static DOTCONF_CB(cb_language);
+static DOTCONF_CB(cb_logFile);
+static DOTCONF_CB(cb_logLevel);
+static DOTCONF_CB(cb_speakupCharacters);
+static DOTCONF_CB(cb_speakupChartab);
+static DOTCONF_CB(cb_speakupCoding);
+static DOTCONF_CB(cb_speakupDevice);
 
-/* Add dotconf configuration option */
-configoption_t *add_config_option(configoption_t * options,
-				  int *num_config_options, char *name, int type,
-				  dotconf_callback_t callback, info_t * info,
-				  unsigned long context)
+/*
+ * Initialize the array of configuration options.
+ */
+static const configoption_t configOptions[] = {
+	{"DontInitTables", ARG_TOGGLE, cb_dontInitTables, NULL, CTX_ALL, },
+	{"Language", ARG_STR, cb_language, NULL, CTX_ALL, },
+	{"LogFile", ARG_STR, cb_logFile, NULL, CTX_ALL, },
+	{"LogLevel", ARG_INT, cb_logLevel, NULL, CTX_ALL, },
+	{"SpeakupCharacters", ARG_STR, cb_speakupCharacters, NULL, CTX_ALL, },
+	{"SpeakupChartab", ARG_STR, cb_speakupChartab, NULL, CTX_ALL, },
+	{"SpeakupCoding", ARG_STR, cb_speakupCoding, NULL, CTX_ALL, },
+	{"SpeakupDevice", ARG_STR, cb_speakupDevice, NULL, CTX_ALL, },
+	LAST_OPTION
+};
+
+static FUNC_ERRORHANDLER(errorhandler)
 {
-	configoption_t *opts;
-
-	(*num_config_options)++;
-	opts = (configoption_t *) realloc(options, (*num_config_options)
-					  * sizeof(configoption_t));
-	opts[*num_config_options - 1].name = strdup(name);
-	opts[*num_config_options - 1].type = type;
-	opts[*num_config_options - 1].callback = callback;
-	opts[*num_config_options - 1].info = info;
-	opts[*num_config_options - 1].context = context;
-	return opts;
+	LOG(2, "Configuration file error: %s\n", msg);
+	return 0;
 }
 
-/* Free all configuration options. */
-void free_config_options(configoption_t * opts, int *num)
+static DOTCONF_CB(cb_dontInitTables)
 {
-	int i = 0;
-
-	if (opts == NULL)
-		return;
-
-	for (i = 0; i <= (*num) - 1; i++) {
-		free((char *)opts[i].name);
+	if (options.dont_init_tables_set != COMMAND_LINE) {
+		LOG(3, "setting %s to %i\n", cmd->name, cmd->data.value);
+		options.dont_init_tables = cmd->data.value;
+		options.dont_init_tables_set = CONFIG_FILE;
+		LOG(3, "setting %s has value %i\n", cmd->name, cmd->data.value);
 	}
-	free(opts);
-	*num = 0;
-	opts = NULL;
-}
-
-/* == CALLBACK DEFINITION MACROS == */
-
-#define DOTCONF_CB_STR(name, arg) \
-   DOTCONF_CB(cb_ ## name) \
-   { \
-       assert(cmd->data.str != NULL); \
-       if (options.arg ## _set != COMMAND_LINE){ \
-           LOG(3, "Setting " #name " to %s", cmd->data.str); \
-           options.arg = strdup(cmd->data.str); \
-           options.arg ## _set = CONFIG_FILE; \
-           LOG(3, "Setting " #name " set to %d", options.arg ## _set); \
-       } \
-       return NULL; \
-   }
-
-#define DOTCONF_CB_INT(name, arg, cond, str) \
-   DOTCONF_CB(cb_ ## name) \
-   { \
-       int val = cmd->data.value; \
-       if (!(cond)) FATAL(-1, str); \
-       if (options.arg ## _set != COMMAND_LINE){ \
-            LOG(3, "Setting " #name " to %d", val); \
-            options.arg = val; \
-            options.arg ## _set = CONFIG_FILE; \
-       } \
-       return NULL; \
-   }
-
-/* == CALLBACK DEFINITIONS == */
-DOTCONF_CB_INT(LogLevel, log_level, ((val >= 0) && (val <= 5)),
-	       "Log level must be between 1 (least verbose) and 5 (very verbose, "
-	       "only for debugging).");
-DOTCONF_CB_STR(LogFile, log_file_name);
-DOTCONF_CB_STR(SpeakupDevice, speakup_device);
-DOTCONF_CB_STR(SpeakupChartab, speakup_chartab);
-DOTCONF_CB_STR(SpeakupCharacters, speakup_characters);
-DOTCONF_CB_STR(SpeakupCoding, speakup_coding);
-DOTCONF_CB_STR(Language, language);
-DOTCONF_CB_INT(DontInitTables, dont_init_tables,
-	       (val == 0) || (val == 1),
-	       "DontInitTables must be either 0 or 1.");
-
-DOTCONF_CB(cb_unknown)
-{
-	LOG(2, "Unknown option in configuration!");
 	return NULL;
 }
 
-/* == LOAD CALLBACKS == */
-
-#define ADD_CONFIG_OPTION(name, arg_type) \
-   options = add_config_option(options, num_options, #name, arg_type, cb_ ## name, 0, 0);
-
-#define ADD_LAST_OPTION() \
-   options = add_config_option(options, num_options, "", 0, NULL, NULL, 0);
-
-configoption_t *load_config_options(int *num_options)
+static DOTCONF_CB(cb_language)
 {
-	configoption_t *options = NULL;
+	assert(cmd->data.str);
+	if (options.language_set != COMMAND_LINE) {
+		LOG(3, "setting %s to %s\n", cmd->name, cmd->data.str);
+		free(options.language);
+		options.language = strdup(cmd->data.str);
+		options.language_set = CONFIG_FILE;
+		LOG(3, "setting %s has value %s\n", cmd->name, cmd->data.str);
+	}
+	return NULL;
+}
 
-	ADD_CONFIG_OPTION(LogLevel, ARG_INT);
-	ADD_CONFIG_OPTION(LogFile, ARG_STR);
-	ADD_CONFIG_OPTION(SpeakupDevice, ARG_STR);
-	ADD_CONFIG_OPTION(SpeakupCoding, ARG_STR);
-	ADD_CONFIG_OPTION(SpeakupChartab, ARG_STR);
-	ADD_CONFIG_OPTION(SpeakupCharacters, ARG_STR);
-	ADD_CONFIG_OPTION(Language, ARG_STR);
-	ADD_CONFIG_OPTION(DontInitTables, ARG_TOGGLE);
+static DOTCONF_CB(cb_logFile)
+{
+	assert(cmd->data.str);
+	if (options.log_file_name_set != COMMAND_LINE) {
+		LOG(3, "setting %s to %s\n", cmd->name, cmd->data.str);
+		free(options.log_file_name);
+		options.log_file_name = strdup(cmd->data.str);
+		options.log_file_name_set = CONFIG_FILE;
+		LOG(3, "setting %s has value %s\n", cmd->name, cmd->data.str);
+	}
+	return NULL;
+}
+ 
+static DOTCONF_CB(cb_logLevel)
+{
+	if ((cmd->data.value < 1 ) || (cmd->data.value > 5))
+		FATAL(-1, "Log level must be between 1 and 5");
+	if (options.log_level_set != COMMAND_LINE) {
+		LOG(3, "setting %s to %i\n", cmd->name, cmd->data.value);
+		options.log_level = cmd->data.value;
+		options.log_level_set = CONFIG_FILE;
+		LOG(3, "setting %s has value %i\n", cmd->name, cmd->data.value);
+	}
+	return NULL;
+}
 
-	ADD_LAST_OPTION();
+static DOTCONF_CB(cb_speakupCharacters)
+{
+	assert(cmd->data.str);
+	if (options.speakup_characters_set != COMMAND_LINE) {
+		LOG(3, "setting %s to %s\n", cmd->name, cmd->data.str);
+		free(options.speakup_characters);
+		options.speakup_characters = strdup(cmd->data.str);
+		options.speakup_characters_set = CONFIG_FILE;
+		LOG(3, "setting %s has value %s\n", cmd->name, cmd->data.str);
+	}
+	return NULL;
+}
 
-	return options;
+static DOTCONF_CB(cb_speakupChartab)
+{
+	assert(cmd->data.str);
+	if (options.speakup_chartab_set != COMMAND_LINE) {
+		LOG(3, "setting %s to %s\n", cmd->name, cmd->data.str);
+		free(options.speakup_chartab);
+		options.speakup_chartab = strdup(cmd->data.str);
+		options.speakup_chartab_set = CONFIG_FILE;
+		LOG(3, "setting %s has value %s\n", cmd->name, cmd->data.str);
+	}
+	return NULL;
+}
 
+static DOTCONF_CB(cb_speakupCoding)
+{
+	assert(cmd->data.str);
+	if (options.speakup_coding_set != COMMAND_LINE) {
+		LOG(3, "setting %s to %s\n", cmd->name, cmd->data.str);
+		free(options.speakup_coding);
+		options.speakup_coding = strdup(cmd->data.str);
+		options.speakup_coding_set = CONFIG_FILE;
+		LOG(3, "setting %s has value %s\n", cmd->name, cmd->data.str);
+	}
+	return NULL;
+}
+
+static DOTCONF_CB(cb_speakupDevice)
+{
+	assert(cmd->data.str);
+	if (options.speakup_device_set != COMMAND_LINE) {
+		LOG(3, "setting %s to %s\n", cmd->name, cmd->data.str);
+		free(options.speakup_device);
+		options.speakup_device = strdup(cmd->data.str);
+		options.speakup_device_set = CONFIG_FILE;
+		LOG(3, "setting %s has value %s\n", cmd->name, cmd->data.str);
+	}
+	return NULL;
 }
 
 void load_configuration(void)
 {
-	configfile_t *configfile = NULL;
-	int dc_num_options = 0;
-	configoption_t *dc_options = NULL;
+	configfile_t *configfile;
 
-	/* Load new configuration */
-	dc_options = load_config_options(&dc_num_options);
-
-	configfile = dotconf_create(options.config_file_name, dc_options,
-								0, CASE_INSENSITIVE);
+	configfile = dotconf_create(options.config_file_name, configOptions,
+		NULL, CASE_INSENSITIVE);
 	if (!configfile) {
 		LOG(0, "Error opening config file\n");
-		return;
+		exit(1);
 	}
+	configfile->errorhandler = (dotconf_errorhandler_t) errorhandler;
 	if (dotconf_command_loop(configfile) == 0)
 		FATAL(-1, "Error reading config file\n");
 	dotconf_cleanup(configfile);
-	free_config_options(dc_options, &dc_num_options);
-	LOG(1, "Configuration has been read from \"%s\"",
-		options.config_file_name);
+	LOG(1, "Configuration has been read from \"%s\"", options.config_file_name);
 }
